1b12f4028897375b4cdb635096b2f280396a1803
llSaborioll <57020420+llSaborioll@users.noreply.github.com>
2019-11-17 16:33:29 -0600

    Add files via upload

---
+++ mainInv/src/maininv/MainInv.java
@@ -0,0 +1,11 @@
+package maininv;
+
+import java.util.*;
+
+public class MainInv {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ mainInv/src/marcaZap.java
@@ -0,0 +1,7 @@
+public class marcaZap {
+    String zapato1 = "Dr Martens";
+    String zapato2 = "Nike";
+    String zapato3 = "Vans";
+    String zapato4 = "";
+    
+}
